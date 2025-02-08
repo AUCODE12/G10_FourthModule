c1765e94528f03477421fd219060743c6ab386d0
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2025-02-08 14:42:41 +0500

    update

---
+++ 4.1/Instagram/Instagram.Dal/EntityConfigurations/CommentConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Instagram.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Instagram.Dal.EntityConfigurations;
+
+public class CommentConfiguration : IEntityTypeConfiguration<Comment>
+{
+    public void Configure(EntityTypeBuilder<Comment> builder)
+    {
+        builder.ToTable("Comment");
+
+        builder.HasKey(c => c.CommentId);
+
+        builder.Property(c => c.Body)
+            .IsRequired(true)
+            .HasMaxLength(200);
+
+        builder.Property(c => c.CreatedTime)
+            .IsRequired(true);
+
+        //builder.HasOne(c => c.ParentComment)
+        //    .WithMany(c => c.Replies)
+        //    .HasForeignKey(c => c.ParentCommentId);
+
+        builder.HasOne(c => c.Post)
+            .WithMany(p => p.Comments)
+            .HasForeignKey(c => c.PostId)
+            .OnDelete(DeleteBehavior.NoAction);
+
+        builder.HasOne(c => c.ParentComment)
+            .WithMany()
+            .HasForeignKey(c => c.ParentCommentId)
+            .OnDelete(DeleteBehavior.NoAction);
+    }
+}
